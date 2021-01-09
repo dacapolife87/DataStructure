a0a1a0d58a6d9cfd0d5bc3b8ef3558b72b2ad7c1
hJJANG <dacapolife87@gmail.com>
2021-01-10 01:11:06 +0900

    Initial commit

---
+++ src/main/java/list/linkerlist/LinkedList.java
@@ -0,0 +1,63 @@
+package list.linkerlist;
+
+public class LinkedList<T> {
+
+    Node header;
+
+    static class Node<T> {
+        T data;
+        Node next = null;
+    }
+
+    public LinkedList() {
+        this.header = new Node();
+    }
+
+    public void add(T data) {
+        Node endNode = new Node();
+        endNode.data = data;
+
+        Node node = header;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = endNode;
+    }
+
+    public void remove(T data) {
+        Node node = header;
+        while (node.next != null) {
+            if(node.next.data.equals(data)) {
+                node.next = node.next.next;
+            }else{
+                node = node.next;
+            }
+        }
+    }
+
+    public void retrieve() {
+        Node node = header.next;
+        while (node.next != null) {
+            System.out.print(node.data + " -> ");
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+
+    public int size() {
+        int listCount = 0;
+        Node node = header;
+        while (node.next != null) {
+            listCount++;
+            node = node.next;
+        }
+
+        return listCount;
+    }
+
+    public boolean isEmpty() {
+        return header.next == null;
+    }
+}
+
+
---
+++ src/test/java/list/linkerlist/LinkedListTest.java
@@ -0,0 +1,46 @@
+package list.linkerlist;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @Test
+    @DisplayName("링크드리스트 사이즈 및 Empty 테스트")
+    void listSizeTest() {
+        LinkedList<Integer> list = new LinkedList<>();
+        assertEquals(0, list.size());
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    @DisplayName("링크드리스트 노드 추가 테스트")
+    void addNodeTest() {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.retrieve();
+        assertEquals(4, list.size());
+    }
+
+    @Test
+    @DisplayName("링크드리스트 노드 삭제 테스트")
+    void removeLinkedList() {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.retrieve();
+        list.remove(3);
+
+        assertEquals(3, list.size());
+    }
+
+
+}
