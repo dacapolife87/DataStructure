777cea1e5d266da8bee14503ac728aef2ba0f3c6
hJJANG <dacapolife87@gmail.com>
2021-01-17 11:16:21 +0900

    feat : 퀵소트 구현 및 Test

---
+++ src/main/java/algorithm/search/Quicksort.java
@@ -0,0 +1,47 @@
+package algorithm.search;
+
+public class Quicksort {
+
+    public void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int start, int end) {
+        int part2 = partition(arr, start, end);
+        if(start < part2 - 1) {
+            quickSort(arr, start, part2 - 1);
+        }
+        if(part2 < end) {
+            quickSort(arr, part2, end);
+        }
+    }
+
+    private int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start+end) / 2];
+        while (start <= end) {
+            while (arr[start] < pivot ) start++;
+            while (arr[end] > pivot ) end--;
+
+            if(start <= end) {
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    private void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+
+    public void printArray(int[] arr) {
+        for (int data : arr) {
+            System.out.print(data + ", ");
+        }
+        System.out.println("");
+    }
+
+}
---
+++ src/test/java/algorithm/search/QuicksortTest.java
@@ -0,0 +1,24 @@
+package algorithm.search;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuicksortTest {
+
+    @Test
+    @DisplayName("퀵소트후 제대로 정렬되었는지 확인")
+    void sort() {
+        int[] arr = {3,9,4,7,0,5,1,6,8,2};
+
+        Quicksort sort = new Quicksort();
+        sort.printArray(arr);
+        sort.quickSort(arr);
+        sort.printArray(arr);
+        for (int i=0; i < arr.length; i++) {
+            assertEquals(i, arr[i]);
+        }
+    }
+
+}
